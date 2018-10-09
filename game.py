6361d6f01ecea2314ad63f2456c6f83fdf4fa1ed
Ragnar-H <ragnarhardarson90@gmail.com>
2018-10-09 18:16:42 +0200

    Validate Int and in range at the same time

--- game.py
+++ game.py
@@ -47,17 +47,13 @@ def get_current_player(num):
 
 
 def get_coordinates():
-    x_coord = raw_input('Choose a column?\n')
-    while not x_coord.isdigit():
-        x_coord = raw_input('please input an integer?\n')
-    while x_coord not in range(0,len(BOARD)):
+    x_coord = raw_input('which column\n')
+    while not x_coord.isdigit() and int(x_coord) not in range(0, len(BOARD)):
         x_coord = int(raw_input('please select a valid column?\n'))
-
-    y_coord = raw_input('Choose a row?\n')
-    while not y_coord.isdigit():
-        y_coord = raw_input('please input an integer?\n')
-    while y_coord not in range(0,len(BOARD)):
+    y_coord = raw_input('which row?\n')
+    while not y_coord.isdigit() and int(y_coord) not in range(0, len(BOARD)):
         y_coord = int(raw_input('please select a valid row?\n'))
+
     return [x_coord, y_coord]
 
 
